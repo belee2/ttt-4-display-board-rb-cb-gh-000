9d0724f4fdf65620adf948f5884f95100f7ef52e
hamoum belaid <spider-cool@live.fr>
2017-02-28 06:17:34 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(tab)
+  puts(" #{tab[0]} | #{tab[1]} | #{tab[2]} ")
+  puts("-----------")
+  puts(" #{tab[3]} | #{tab[4]} | #{tab[5]} ")
+  puts("-----------")
+  puts(" #{tab[6]} | #{tab[7]} | #{tab[8]} ")
+end
